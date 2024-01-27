3c44a25f5fbbc35620cde816d4209093c268e4b2
Asllan Makaj <animakaj7@gmail.com>
2024-01-27 17:33:37 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# type-basedOn_jsonFileKeys
+
+
+
+
+![showCase](https://github.com/asllanmakaj7/type-basedOn_jsonFileKeys/blob/main/img.png?raw=true)
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import jsonFile from "../app.json"
+import jsonFile from "../app.json";
 type T = typeof jsonFile;
 type K = keyof T; 
-type F = (key: K ) => void
+type F = (key: K ) => void;
 const func : F= (key) =>{
-    console.log(key),
     console.table([[key,jsonFile[key]]])
-
+;
 }
 
-func("b")
-
-
+func("b");
